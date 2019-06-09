cb30bcffa6dfb742bd4f7ed4357e93e45a2126aa
Vera Boycheva <vboi4eva@gmail.com>
2019-06-09 17:47:17 +0300

    userentity

--- src/main/java/model/UserEntity.java
+++ src/main/java/model/UserEntity.java
@@ -1,23 +1,32 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import org.springframework.security.core.GrantedAuthority;
+
 @Entity
-public class User {
+public class UserEntity {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	private String email;
 	private String firstName;
 	private String lastName;
+	private String password;
+	@ElementCollection(targetClass=String.class)
+	private Collection<GrantedAuthority> grantedAuthoritiesList = new ArrayList<>();
 
-	public User() {
+	public UserEntity() {
 	}
 
-	public User(String email, String firstName, String lastName) {
+	public UserEntity(String email, String firstName, String lastName) {
 		this.email = email;
 		this.firstName = firstName;
 		this.lastName = lastName;
@@ -58,5 +67,21 @@ public class User {
 
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Collection<GrantedAuthority> getGrantedAuthoritiesList() {
+		return grantedAuthoritiesList;
+	}
+
+	public void setGrantedAuthoritiesList(Collection<GrantedAuthority> grantedAuthoritiesList) {
+		this.grantedAuthoritiesList = grantedAuthoritiesList;
 	}
 }
